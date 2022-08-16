89c353dc1dcf07efe6cfe32a0b162c89c3d93314
Yimesgen Morka <dochomorka@gmail.com>
2022-08-16 08:52:28 +0300

    added non nullable constraint

--- app.py
+++ app.py
@@ -61,9 +61,9 @@ class Show(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     start_time = db.Column(db.DateTime, nullable=False)
-    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=True)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
     artist_id = db.Column(
-        db.Integer, db.ForeignKey('Artist.id'), nullable=True)
+        db.Integer, db.ForeignKey('Artist.id'), nullable=False)
 
 
 class Artist(db.Model):
