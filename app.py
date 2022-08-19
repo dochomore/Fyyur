f3af14778ad456adc3386edce7115028511892ea
Yimesgen Morka <dochomorka@gmail.com>
2022-08-19 16:20:00 +0300

    implemented populating forms by data came from db

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import json
 from datetime import datetime
 import logging
 from logging import FileHandler, Formatter
+from msilib.schema import Error
+from unicodedata import name
 
 import babel
 import dateutil.parser
@@ -372,21 +374,28 @@ def show_artist(artist_id):
 
 @app.route('/artists/<int:artist_id>/edit', methods=['GET'])
 def edit_artist(artist_id):
-    form = ArtistForm()
-    artist = {
-        "id": 4,
-        "name": "Guns N Petals",
-        "genres": ["Rock n Roll"],
-        "city": "San Francisco",
-        "state": "CA",
-        "phone": "326-123-5000",
-        "website": "https://www.gunsnpetalsband.com",
-        "facebook_link": "https://www.facebook.com/GunsNPetals",
-        "seeking_venue": True,
-        "seeking_description": "Looking for shows to perform at in the San Francisco Bay Area!",
-        "image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80"
-    }
-    # TODO: populate form with fields from artist with ID <artist_id>
+    try:
+        form = ArtistForm()
+        artist = db.session.query(Artist).get(artist_id)
+        if artist:
+            form.name.data = artist['name']
+            form.phone.data = artist['phone']
+            form.genres.data = artist['genres']
+            form.city.data = artist['city']
+            form.state.data = artist['state']
+            form.phone.data = artist['phone']
+            form.website_link.data = artist['website']
+            form.facebook_link.data = artist['facebook_link']
+            form.seeking_venue.data = artist['seeking_venue']
+            form.seeking_description.data = artist['seeking_description']
+            form.image_link.data = artist['image_link']
+        else:
+            raise Exception()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
+
     return render_template('forms/edit_artist.html', form=form, artist=artist)
 
 
@@ -400,22 +409,26 @@ def edit_artist_submission(artist_id):
 
 @app.route('/venues/<int:venue_id>/edit', methods=['GET'])
 def edit_venue(venue_id):
-    form = VenueForm()
-    venue = {
-        "id": 1,
-        "name": "The Musical Hop",
-        "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-        "address": "1015 Folsom Street",
-        "city": "San Francisco",
-        "state": "CA",
-        "phone": "123-123-1234",
-        "website": "https://www.themusicalhop.com",
-        "facebook_link": "https://www.facebook.com/TheMusicalHop",
-        "seeking_talent": True,
-        "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-        "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
-    }
-    # TODO: populate form with values from venue with ID <venue_id>
+    try:
+        form = VenueForm()
+        venue = db.session.query(Venue).get(venue_id)
+        if venue:
+            form.name.data = venue.name
+            form.phone.data = venue.phone
+            form.address.data = venue.address
+            form.city.data = venue.city
+            form.state.data = venue.state
+            form.website_link.data = venue.website_link
+            form.facebook_link.data = venue.facebook_link
+            form.seeking_talent.data = venue.seeking_talent
+            form.seeking_description.data = venue.seeking_description
+            form.image_link.data = venue.image_link
+        else:
+            raise Exception()
+    except:
+        db.session.rollback()
+    finally:
+        db.session.close()
     return render_template('forms/edit_venue.html', form=form, venue=venue)
 
 
