48207ab471941dab446bedb4c562b7e770599a2c
Yimesgen Morka <dochomorka@gmail.com>
2022-08-19 06:40:55 +0300

    implemented /artists/search endpoint

--- app.py
+++ app.py
@@ -318,14 +318,20 @@ def search_artists():
     # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
     # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
     # search for "band" should return "The Wild Sax Band".
-    response = {
-        "count": 1,
-        "data": [{
-            "id": 4,
-            "name": "Guns N Petals",
-            "num_upcoming_shows": 0,
-        }]
-    }
+
+    response = {}
+    keyword = request.form.get('search_term')
+    if keyword:
+        search = "%{}%".format(keyword)
+        artists = Artist.query.filter(Artist.name.ilike(search)).all()
+        response['count'] = len(artists)
+        data = []
+        for artist in artists:
+            upcoming_shows = db.session.query(Show).join(Artist,Show.id == artist.id).filter(Show.start_time > datetime.now()).count()
+            data.append({'id': artist.id, 'name': artist.name,
+                        'num_upcoming_show': upcoming_shows})
+        response['data'] = data
+
     return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
 
 
